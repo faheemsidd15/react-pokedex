f2cbb9e2672ec2bdfc481c6ea8faacb469481f0c
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2019-03-21 10:58:13 -0400

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+// eslint-disable-next-line react/prefer-stateless-function
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<div>Hello world</div>
+			</div>
+		)
+	}
+}
+
+export default App
