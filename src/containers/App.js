4cb8be3aa4d226b99ae22b944992f8c04f7ee587
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2019-03-22 13:01:25 -0400

    images

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,40 @@
-import React, { Component } from "react"
+import React from "react"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import ImageLoader from "react-load-image"
+
+// const pokeImage = {
+// 	pokemon: number => fetch(`../images/pokemon/${number}.png`).then(res => res.url)
+// }
+
+// const getPokemonImage = number => ({
+// 	pokemon: () => import(`../images/pokemon/${number}.png`)
+// })
+
+// getPokemonImage("001").pokemon()
+
+// console.log(getPokemonImage("001").pokemon())
 
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends Component {
+
+function Preloader(props) {
+	return <div>Loading..</div>
+}
+class App extends React.Component {
 	render() {
 		return (
-			<div className="App">
-				<div>Hello world</div>
-			</div>
+			<Container>
+				<Row>
+					<Col md={4} style={{ backgroundColor: "red" }}>
+						<ImageLoader>
+							<img />
+							<div>Error!</div>
+							<Preloader />
+						</ImageLoader>
+					</Col>
+				</Row>
+			</Container>
 		)
 	}
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,35 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from "./containers/App"
 import "./index.css"
-
+import ApolloClient from "apollo-boost"
 import { HashRouter as Router } from "react-router-dom"
+import { ApolloProvider } from "react-apollo"
+//import gql from "graphql-tag"
+
+const client = new ApolloClient({
+	uri: "https://pokeapi-graphiql.herokuapp.com/?"
+})
+
+// client
+// 	.query({
+// 		query: gql`
+// 			{
+// 				pokemon(number: 1) {
+// 					name
+// 				}
+// 			}
+// 		`
+// 	})
+// 	.then(result => console.log(result))
 
 //import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
-	<Router>
-		<App />
-	</Router>,
+	<ApolloProvider client={client}>
+		<Router>
+			<App />
+		</Router>
+	</ApolloProvider>,
 	document.getElementById("root")
 )
 
