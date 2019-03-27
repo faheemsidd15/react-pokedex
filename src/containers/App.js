c4fb5edd5e648afc631086fafb9b10cb91bdaf76
faheemsidd15 <blazefs@gmail.com>
2019-03-27 09:09:13 -0400

    rendering images

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,26 +4,40 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import ImageLoader from "react-load-image"
 
-
 function Preloader(props) {
 	return <div>Loading..</div>
 }
+const getGen = (first, last) => {
+	let pokemonIDX = []
 
-
-
+	for (let i = first; i <= last; i++) {
+		pokemonIDX.push([i] <= 9 ? "00" + [i] + ".png" : [i] + ".png")
+	}
+	return pokemonIDX
+}
 
 class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			pokemonGroup: getGen(1, 151)
+		}
+	}
+
 	render() {
+		console.log(this.state)
 		return (
 			<Container>
 				<Row>
-					<Col md={4} style={{ backgroundColor: "red" }}>
-						<ImageLoader src="images/pokemon/001.png">
-							<img />
-							<div>Error!</div>
-							<Preloader />
-						</ImageLoader>
-					</Col>
+					{this.state.pokemonGroup.map(image => (
+						<Col md={4} style={{ backgroundColor: "red" }} key={image}>
+							<ImageLoader src={`images/pokemon/${image}`}>
+								<img />
+								<div>Error!</div>
+								<Preloader />
+							</ImageLoader>
+						</Col>
+					))}
 				</Row>
 			</Container>
 		)
