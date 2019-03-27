7d06607fd5e7ba0ac6f5912dcadaea4d49622b00
Faheem Siddiqui <faheem.siddiqui@industryweapon.com>
2019-03-26 22:43:17 -0400

    change folder location for images

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,30 +4,21 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import ImageLoader from "react-load-image"
 
-// const pokeImage = {
-// 	pokemon: number => fetch(`../images/pokemon/${number}.png`).then(res => res.url)
-// }
-
-// const getPokemonImage = number => ({
-// 	pokemon: () => import(`../images/pokemon/${number}.png`)
-// })
-
-// getPokemonImage("001").pokemon()
-
-// console.log(getPokemonImage("001").pokemon())
-
-// eslint-disable-next-line react/prefer-stateless-function
 
 function Preloader(props) {
 	return <div>Loading..</div>
 }
+
+
+
+
 class App extends React.Component {
 	render() {
 		return (
 			<Container>
 				<Row>
 					<Col md={4} style={{ backgroundColor: "red" }}>
-						<ImageLoader>
+						<ImageLoader src="images/pokemon/001.png">
 							<img />
 							<div>Error!</div>
 							<Preloader />
